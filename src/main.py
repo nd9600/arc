a33de274c8fdaf4cce87257cc8f187ee03ce7c26
Nathan Douglas <nd9600@gmail.com>
2020-02-29 13:23:18 +0000

    refactored

--- src/main.py
+++ src/main.py
@@ -12,46 +12,52 @@ import matplotlib.pyplot as plt
 from matplotlib import colors
 
 
-def plotOne(ax, i, train_or_test, input_or_output):
-    cmap = colors.ListedColormap(
-        ['#000000', '#0074D9', '#FF4136', '#2ECC40', '#FFDC00',
-         '#AAAAAA', '#F012BE', '#FF851B', '#7FDBFF', '#870C25'])
-    norm = colors.Normalize(vmin=0, vmax=9)
+def plot_grid(axes, input_matrix, title = "") -> None:
+    colour_map = colors.ListedColormap(
+        ['#000000', '#0074D9', '#FF4136', '#2ECC40', '#FFDC00', '#AAAAAA', '#F012BE', '#FF851B', '#7FDBFF', '#870C25']
+    )
+    norm = colors.Normalize(vmin = 0, vmax = 9)
 
-    input_matrix = task[train_or_test][i][input_or_output]
-    ax.imshow(input_matrix, cmap=cmap, norm=norm)
-    ax.grid(True, which='both', color='lightgrey', linewidth=0.5)
-    ax.set_yticks([x - 0.5 for x in range(1 + len(input_matrix))])
-    ax.set_xticks([x - 0.5 for x in range(1 + len(input_matrix[0]))])
-    ax.set_xticklabels([])
-    ax.set_yticklabels([])
-    ax.set_title(train_or_test + ' ' + input_or_output)
+    axes.imshow(input_matrix, cmap = colour_map, norm = norm)
+    axes.grid(True, which = 'both', color = 'lightgrey', linewidth = 0.5)
+    axes.set_yticks([x - 0.5 for x in range(1 + len(input_matrix))])
+    axes.set_xticks([x - 0.5 for x in range(1 + len(input_matrix[0]))])
+    axes.set_xticklabels([])
+    axes.set_yticklabels([])
+    if len(title) > 0:
+        axes.set_title(title)
 
 
-def plotTask(task):
+def plot_task(task):
     """
     Plots the first train and test pairs of a specified task,
     using same color scheme as the ARC app
     """
-    num_train = len(task['train'])
-    fig, axs = plt.subplots(2, num_train, figsize=(3 * num_train, 3 * 2))
-    for i in range(num_train):
-        plotOne(axs[0, i], i, 'train', 'input')
-        plotOne(axs[1, i], i, 'train', 'output')
+    number_of_training_examples = len(task['train'])
+    fig, axes_array = plt.subplots(2, number_of_training_examples, figsize = (3 * number_of_training_examples, 3 * 2))
+    for task_number in range(number_of_training_examples):
+        input_axis = axes_array[0, task_number]
+        input_matrix = task['train'][task_number]['input']
+        plot_grid(input_axis, input_matrix, 'train input')
+
+        output_axis = axes_array[1, task_number]
+        output_matrix = task['train'][task_number]['output']
+        plot_grid(output_axis, output_matrix, 'train output')
     plt.tight_layout()
     plt.show()
 
-    num_test = len(task['test'])
-    fig, axs = plt.subplots(2, num_test, figsize=(3 * num_test, 3 * 2))
-    if num_test == 1:
-        plotOne(axs[0], 0, 'test', 'input')
-        plotOne(axs[1], 0, 'test', 'output')
-    else:
-        for i in range(num_test):
-            plotOne(axs[0, i], i, 'test', 'input')
-            plotOne(axs[1, i], i, 'test', 'output')
-    plt.tight_layout()
-    plt.show()
+    # num_test = len(task['test'])
+    # fig, axs = plt.subplots(2, num_test, figsize=(3 * num_test, 3 * 2))
+    # if num_test == 1:
+    #     plot_one(axs[0], 0, 'test', 'input')
+    #     plot_one(axs[1], 0, 'test', 'output')
+    # else:
+    #     for task_number in range(num_test):
+    #         plot_one(axs[0, task_number], task_number, 'test', 'input')
+    #         plot_one(axs[1, task_number], task_number, 'test', 'output')
+    # plt.tight_layout()
+    # plt.show()
+
 
 rootPath = Path("../")
 data_path = rootPath / "data"
@@ -65,8 +71,8 @@ for i in range(1):
     task_file = str(training_path / training_tasks[i])
 
     with open(task_file, 'r') as f:
-        task = json.load(f)
+        task_json = json.load(f)
 
     print(i)
     print(training_tasks[i])
-    plotTask(task)
+    plot_task(task_json)
