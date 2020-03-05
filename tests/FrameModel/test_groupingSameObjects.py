240db4208f9cca74127946a214eb0addaf24cbb9
Nathan Douglas <nd9600@gmail.com>
2020-03-05 12:30:37 +0000

    made tests properly fail

--- tests/FrameModel/test_groupingSameObjects.py
+++ tests/FrameModel/test_groupingSameObjects.py
@@ -8,6 +8,7 @@ from src.Grid.Grid import Grid
 
 class TestGroupingSameObjects(unittest.TestCase):
     def test_grouping_one_object(self):
+        # tests fail because of IDs
         g = [
             [0, 1, 1],
             [0, 0, 0],
@@ -24,17 +25,20 @@ class TestGroupingSameObjects(unittest.TestCase):
         expected_object_group = expected_grouped_objects[obj.get_object_kind()]
 
         actual_grouped_objects = frame_model.group_same_objects()
-        print(actual_grouped_objects)
+        actual_object_group = actual_grouped_objects[obj.get_object_kind()]
         self.assertEqual(1, len(list(actual_grouped_objects.keys())))
-        self.assertEqual(1, len(expected_object_group))
+        self.assertEqual(1, len(actual_object_group))
+
+        print(expected_grouped_objects)
+        print(actual_grouped_objects)
 
         self.assertEqual(
             list(expected_grouped_objects.keys()),
             list(actual_grouped_objects.keys())
         )
         self.assertEqual(
-            [obj.id],
-            expected_object_group
+            expected_object_group,
+            actual_object_group
         )
 
     def test_grouping_two_objects(self):
@@ -54,18 +58,50 @@ class TestGroupingSameObjects(unittest.TestCase):
         expected_object_group = expected_grouped_objects[obj_a.get_object_kind()]
 
         actual_grouped_objects = frame_model.group_same_objects()
-        print(actual_grouped_objects)
+        actual_object_group = actual_grouped_objects[obj_a.get_object_kind()]
 
         self.assertEqual(1, len(list(actual_grouped_objects.keys())))
-        self.assertEqual(2, len(expected_object_group))
+        self.assertEqual(2, len(actual_object_group))
 
         self.assertEqual(
             list(expected_grouped_objects.keys()),
             list(actual_grouped_objects.keys())
         )
         self.assertEqual(
-            [obj_a.id, obj_b.id],
-            expected_object_group
+            expected_object_group,
+            actual_object_group
+        )
+
+    def test_grouping_two_objects_where_one_is_rotated(self):
+        # todo: fails because rotation isn't taken into account
+        g = [
+            [0, 1, 1],
+            [0, 0, 0],
+            [0, 1, 0],
+            [0, 1, 0],
+        ]
+        grid = Grid(g)
+        frame_model = FrameModel.create_from_grid(grid)
+        obj_a = list(grid.parse_objects().values())[0]
+        obj_b = list(grid.parse_objects().values())[1]
+        expected_grouped_objects: Dict[ObjectKind, List[ObjectId]] = {
+            obj_a.get_object_kind(): [obj_a.id, obj_b.id]
+        }
+        expected_object_group = expected_grouped_objects[obj_a.get_object_kind()]
+
+        actual_grouped_objects = frame_model.group_same_objects()
+        actual_object_group = actual_grouped_objects[obj_a.get_object_kind()]
+
+        self.assertEqual(1, len(list(actual_grouped_objects.keys())))
+        self.assertEqual(2, len(actual_object_group))
+
+        self.assertEqual(
+            list(expected_grouped_objects.keys()),
+            list(actual_grouped_objects.keys())
+        )
+        self.assertEqual(
+            expected_object_group,
+            actual_object_group
         )
 
 
